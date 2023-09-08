c91b3a3b87f245508a7564537b0a652121c699e8
Romanyerom <Romanyerom@github.com>
2023-09-08 06:28:00 -0700

    correction

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This script demonstrates the usage of the Rectangle class.
+It creates a Rectangle instance and prints its type and attributes.
+"""
 
 class Rectangle:
     pass
