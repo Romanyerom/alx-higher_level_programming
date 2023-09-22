76ef6e8e6e47e241a1ecd939a17edf754d20660d
Romanyerom <Romanyerom@github.com>
2023-09-22 05:47:01 -0700

    correction

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,4 +3,11 @@
 It creates a Rectangle instance and prints its type and attributes.
 """
 class Rectangle:
+    """
+    This is a simple Rectangle class.It represents a rectangle with no additional attributes or methods.
+    """
     pass
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
