4eb2ff7cc4039a369b2984287e622722ee31a04e
Romanyerom <Romanyerom@github.com>
2023-09-30 10:22:55 -0700

    remaining codes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Defines an inherited list class MyList."""
 
+
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
-
-
     def print_sorted(self):
         """
         Print the list in ascending sorted order.
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,25 +3,15 @@
 
 
 def is_same_class(obj, a_class):
-    """
-    Check if an object is an instance of a specified class.
+    """Check if an object is exactly an instance of a given class.
 
     Args:
-        obj (object): The object to check.
-        a_class (type): The class to compare with.
-
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        bool: True if the object is an instance
-        of the specified class, False otherwise.
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
-    return type(obj) is a_class
-
-
-if __name__ == "__main__":
-    a = 1
-    if is_same_class(a, int):
-        print("{} is an instance of the class {}".format(a, int.__name__))
-        if is_same_class(a, float):
-            print("{} is an instance of the class {}".format(a, float.__name__))
-            if is_same_class(a, object):
-                print("{} is an instance of the class {}".format(a, object.__name__))
+    if type(obj) == a_class:
+        return True
+    return False
