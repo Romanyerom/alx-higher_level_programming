4b3d48935c089ad6602a8ac84f75dde4da25764d
Romanyerom <Romanyerom@github.com>
2023-12-02 07:48:31 -0800

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if a URL is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Use curl to send a request and display the size of the response body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
