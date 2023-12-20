857ab745432465b587e3025bc1b8488e879edf43
Romanyerom <Romanyerom@github.com>
2023-12-20 05:51:56 -0800

    Error code #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -15,9 +15,9 @@ def fetch_status():
         body = response.read()
 
     print("Body response:")
-    print("    - type:", type(body))
-    print("    - content:", body)
-    print("    - utf8 content:", body.decode("utf-8"))
+    print(" - type:", type(body))
+    print(" - content:", body)
+    print(" - utf8 content:", body.decode("utf-8"))
 
 if __name__ == "__main__":
     fetch_status()
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
-        print("Your email is:", body)
+        print("Email:", body)
