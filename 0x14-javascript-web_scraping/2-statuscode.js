4cee707495a1cbd8e11d3572c9e39b74b59b27c9
Romanyerom <Romanyerom@github.com>
2024-02-03 08:23:10 -0800

    done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
